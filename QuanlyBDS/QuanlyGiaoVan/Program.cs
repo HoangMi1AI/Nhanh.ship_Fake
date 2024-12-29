71abc8911426da994c956c8c6f3e975c6fa66480
Manh Nguyễn <manh.nd2824@gmail.com>
2024-12-29 11:15:39 +0700

    thêm cái form quản lý đơn hàng

--- QuanlyBDS/QuanlyGiaoVan/Program.cs
+++ QuanlyBDS/QuanlyGiaoVan/Program.cs
@@ -11,7 +11,7 @@ namespace QuanlyGiaoVan
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FormTaoDonHang());
+            Application.Run(new FormQuanLyDonHang());
         }
     }
 }
