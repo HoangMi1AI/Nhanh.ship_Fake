e8486f6dcc25cfa5331d014628a7314bb0be0f30
Manh Nguyễn <manh.nd2824@gmail.com>
2024-12-31 12:37:08 +0700

    thêm back

--- QuanlyBDS/QuanlyGiaoVan/obj/Debug/net8.0-windows/QuanlyGiaoVan.AssemblyInfo.cs
+++ QuanlyBDS/QuanlyGiaoVan/obj/Debug/net8.0-windows/QuanlyGiaoVan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanlyGiaoVan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3441fd3859250c911c099edf18806980002c6d9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c54942cbe77315b3bd96772d36ee3ce3363b1f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanlyGiaoVan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanlyGiaoVan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
